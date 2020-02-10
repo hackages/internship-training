3cb6ce77736fdeaf36398960deef76fa74ef001b
Jxwad <jawad@hackages.io>
2020-02-10 13:36:46 +0100

    init home component

---
+++ traininginternship/src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import React, {useEffect} from "react";
+import fakeData from '../../fakeData';
+
+export const Home = () => {
+
+    return (
+        <>
+            <h1>Trainings</h1>
+            <ul>
+
+                <li></li>
+            </ul>
+
+        </>
+    );
+};
