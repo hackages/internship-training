762a44f0033509771d87681ae2e236a69bd2b333
Alexis Rifaut <alexis@c02w33kxg8wn.local>
2020-02-12 16:44:48 +0100

    Correction commande build

---
+++ traininginternship/build/precache-manifest.b3013a83c201eabc24d1098320f5a9f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f2b52dcdd68f6ca62ed7ca04445994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ba11ad4fbebd22322f6",
+    "url": "/static/css/main.373a73b1.chunk.css"
+  },
+  {
+    "revision": "8fd128f67a0179f5faa1",
+    "url": "/static/js/2.0d5c3de2.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.0d5c3de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba11ad4fbebd22322f6",
+    "url": "/static/js/main.ef23168c.chunk.js"
+  },
+  {
+    "revision": "9798381974d5bc54b4be",
+    "url": "/static/js/runtime-main.893946ad.js"
+  }
+]);
