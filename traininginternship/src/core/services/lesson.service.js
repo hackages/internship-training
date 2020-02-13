e167abe57a541c8691277dbbc3d15eceb37ba350
Alexis Rifaut <alexis@c02w33kxg8wn.local>
2020-02-13 09:26:44 +0100

    Diverses modifications

--- traininginternship/src/core/services/lesson.service.js
+++ traininginternship/src/core/services/lesson.service.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
+const config = {
+    headers: {'crossdomain': 'true'}
+};
+
 function getLessons() {
-    return axios.get(process.env.REACT_APP_URL_BASE + "activities?types[]=event&types[]=learningPath&sortBy=dates&sortByType=asc&periods[]=upcoming&price=true&privacies[]=public&published=true&toPopulate[]=topics&toPopulate[]=location")
+    return axios.get(process.env.REACT_APP_URL_BASE + "activities?types[]=event&types[]=learningPath&sortBy=dates&sortByType=asc&periods[]=upcoming&price=true&privacies[]=public&published=true&toPopulate[]=topics&toPopulate[]=location", config)
         .then(res => {
             return res.data;
         })
 }
 
 function getLesson(id) {
-    return axios.get(process.env.REACT_APP_URL_BASE + "activities/" + id + "?toPopulate[]=subscribers&toPopulate[]=topics&toPopulate[]=location&toPopulate[]=groups&toPopulate[]=owner&toPopulate[]=mentors&toPopulate[]=contacts&toPopulate[]=steps.activities")
+    return axios.get(process.env.REACT_APP_URL_BASE + "activities/" + id + "?toPopulate[]=subscribers&toPopulate[]=topics&toPopulate[]=location&toPopulate[]=groups&toPopulate[]=owner&toPopulate[]=mentors&toPopulate[]=contacts&toPopulate[]=steps.activities", config)
         .then(res => {
             return res.data;
         })
